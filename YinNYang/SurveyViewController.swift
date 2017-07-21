b637698f4de08395bcf160bb714990aa7fda4bfd
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-07-20 20:12:56 -0400

    Committing SurveyViewController

---
+++ YinNYang/SurveyViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SurveyViewController.swift
+//  YinNYang
+//
+//  Created by Alikeju Adejo on 7/19/17.
+//  Copyright © 2017 Alikeju Adejo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SurveyViewController: UIViewController{
+    
+    
+
+    @IBOutlet weak var `continue`: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    
+    
+
+   
+}
