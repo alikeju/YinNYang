5c275e7e63f68d75cde5cfcc096e5d0dc3d9ed49
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-07-20 20:15:09 -0400

    Committing the AppDelegate

--- YinNYang/AppDelegate.swift
+++ YinNYang/AppDelegate.swift
@@ -4,9 +4,10 @@
 //
 //  Created by Alikeju Adejo on 7/18/17.
 //  Copyright © 2017 Alikeju Adejo. All rights reserved.
-//
+// 
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
